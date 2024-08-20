a2fca77ac0d9d193e9b6a458a193f592b925c0c0
Mohammed Haaroon <md.haaroon0710@gmail.com>
2024-08-20 20:06:09 +0530

    Corrected the marking lines

--- maincode.py
+++ maincode.py
@@ -4,15 +4,33 @@ import pickle
 import cvzone
 
 
-c=cv2.VideoCapture('Parking footage.mp4')
+c=cv2.VideoCapture('Park lot footage.mp4')
 
-#displaying the video fead by capturing each frames
+with open("Carpos", "rb") as file:
+    pos=pickle.load(file)
+
+width, height= 107, 48
+
+def check():
+    for p in pos:
+        x,y = p
+        crop=img[y:y+height, x:x+width] #crops each block of parking space
+        cv2.imshow("Croppedvideo",crop) #shows each cropped video
+        
+#displaying the video feed by capturing each frames
 while True:
     
     #looping the video
     if c.get(cv2.CAP_PROP_POS_FRAMES)== c.get(cv2.CAP_PROP_FRAME_COUNT): #current position and total no of frames in the video is compared
         c.set(cv2.CAP_PROP_POS_FRAMES,0) #resetting the frames to 0 once the video ends
-    
     success, img= c.read()
-    cv2.imshow("Image", img)
+    
+    grayimg=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)#converting it to gray image since gray images work accurately for object detection
+    blur=cv2.GaussianBlur(grayimg, (3,3),1) #smoothing out the edges
+    #converting to binary image
+    imgThres = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,25,16)
+    check()
+    for p in pos:
+        cv2.rectangle(img,p,(p[0]+width,p[1]+height),(255,0,0), 2)
+    cv2.imshow("Image thres", imgThres)
     cv2.waitKey(10)
--- spacepicker.py
+++ spacepicker.py
@@ -3,7 +3,7 @@ import pickle
 
 
 
-width, height= 195, 70 #found out the width and height by trial and error method
+width, height = 107, 48 #found out the width and height by trial and error method
 try:
     with open("Carpos", "rb") as file:
         pos=pickle.load(file) #if pos is already available. if not done then the image will be overwritten everytime.
@@ -21,12 +21,13 @@ def mouseclick(events, x, y, flags, params):
                 pos.pop(i)
                 break
     with open("Carpos", "wb") as file:
-        pickle.dump(pos,file) #dumping pos in 
+        pickle.dump(pos,file) #dumping pos in file
 
 
 while True:
-    #cv2.rectangle(img,(55,82),(255,166),(0,0,255), 2)  used trial and error to get the correct outline
-    img=cv2.imread("Car image.png")
+    img=cv2.imread("parking lot image.png")
+    #cv2.rectangle(img,(45,89),(162,144),(0,0,255), 2)  #used trial and error to get the correct outline
+    
     for p in pos:
         cv2.rectangle(img,p,(p[0]+width,p[1]+height),(255,0,0), 2) 
     cv2.imshow("Car parking" ,img)
